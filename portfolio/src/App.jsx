7d33c9b9714d1b81702760b67d2ad6586564e688
Cyril KIM <65430762+KimTechpreneur@users.noreply.github.com>
2023-06-24 10:18:25 +0100

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -17,7 +17,7 @@ import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
-import Testimonial from './components/testimonial/testimonial2';
+import Testimonial from './components/testimonial/Testimonial';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
